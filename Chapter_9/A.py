4c855517620b550701d25889fe02aa98397f9b0d
Yugyeong Park <bonbonly7@naver.com>
2023-04-09 21:53:51 +0900

    09. 함수와 모듈 Section 05 모듈 모듈의 생성과 사용

---
+++ Chapter_9/A.py
@@ -0,0 +1,5 @@
+import Module1
+
+Module1.func1()
+Module1.func2()
+Module1.func3()
---
+++ Chapter_9/Module1.py
@@ -0,0 +1,8 @@
+def func1() :
+    print("Module1.py의 func1()이 호출됨.")
+
+def func2() :
+    print("Module1.py의 func2()가 호출됨.")
+
+def func3() :
+    print("Module1.py의 func3()이 호출됨.")
